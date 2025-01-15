79ad44592fd7a499f78a20146e3a70c7a0a51540
Gabrielle <ms.grodarte@gmail.com>
2025-01-15 09:11:30 -0800

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves):
+        print(f"{index + 1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [call.title() + "!" for call in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    if "cheddar" in foods:
+        return "cheddar"
+    elif "gouda" in foods:
+        return "gouda"
+    elif "camembert" in foods:
+        return "camembert"
+    return None
